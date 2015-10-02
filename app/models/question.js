eb846b5b0f842fba362a0c35210e22b7c2bd9ae3
Guest User <student@epicodus.com>
2015-10-02 10:56:36 -0700

    Questions and authors appear on home page

--- app/models/question.js
+++ app/models/question.js
@@ -1,5 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+  email: DS.attr(),
+  longQuestion: DS.attr(),
+  shortQuestion: DS.attr(),
+  topic: DS.attr(),
+  username: DS.attr()
 });
