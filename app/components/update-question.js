cf335e787ea89afd60e527503b3b1d6b63f38e62
Guest User <student@epicodus.com>
2015-10-02 13:08:13 -0700

    Add update functionality to individual question pages

---
+++ app/components/update-question.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  updateQuestionForm: false,
+  actions: {
+    updateQuestionForm() {
+      this.set('updateQuestionForm', true);
+    },
+    update(question) {
+      var params = {
+        username: this.get('username') ? this.get('username') : "",
+        email: this.get('email') ? this.get('email') : "",
+        topic: this.get('topic') ? this.get('topic') : "",
+        shortQuestion: this.get('shortQuestion') ? this.get('shortQuestion') : "",
+        longQuestion: this.get('longQuestion') ? this.get('longQuestion') : "",
+      };
+      this.set('updateQuestionForm', false);
+      this.sendAction('update', question, params);
+    }
+  }
+});
