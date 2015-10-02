44794fe4402647ba0626c59fbc121f540cb75680
Guest User <student@epicodus.com>
2015-10-02 16:06:06 -0700

    Add styling

--- app/routes/question.js
+++ app/routes/question.js
@@ -16,13 +16,12 @@ export default Ember.Route.extend({
     },
     saveAnswer(params) {
       var newAnswer = this.store.createRecord('answer', params);
-      debugger;
       var question = params.question;
       question.get('answers').addObject(newAnswer);
       newAnswer.save().then(function() {
         return question.save();
       });
-        this.transitionTo('question', params.question);
+        this.transitionTo('question');
     },
   }
 });
